1bff9917fbed840a88c03d0e9ba0040f11f812c2
Alex Ívar <alexivar65@gmail.com>
2015-12-07 19:32:06 +0000

    horizontal win test added, duplicate move test added and more

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,20 +1,72 @@
+'use strict';
+
+var _ = require('lodash');
+
 module.exports = function tictactoeCommandHandler(events) {
-  var gameCreatedEvent = events[0];
+  var gameState = {
+    gameCreatedEvent : events[0],
+    board: [['','',''],['','',''],['','','']]
+  };
+
+  var eventHandlers={
+    'MoveMade': function(event){
+      gameState.board[event.x][event.y] = event.side;
+    }
+  };
+
+  _.each(events, function(event){
+    var eventHandler = eventHandlers[event.event];
+    if(eventHandler) eventHandler(event);
+  });
+
+  const HasWon = function(cmd) {
+        let sum = 0;
+        // Vertical check
+        for (let i = 0; i < 3; ++i) {
+            if (gameState.board[cmd.x][i] === cmd.side) sum++;
+        }
+
+        if (sum === 2) return true;
+
+        sum = 0;
+        // Horizontal check
+        for (let i = 0; i < 3; ++i) {
+            if (gameState.board[i][cmd.y] === cmd.side) sum++;
+        }
+
+        if (sum === 2) return true;
+
+        sum = 0;
+        // Diagonal check
+        for (let i = 0; i < 3; ++i) {
+            if (gameState.board[i][i] === cmd.side) sum++;
+        }
+        if (sum === 2) return true;
+        sum = 0;
+        for (let x = 2, y = 0; x > -1 && y < 3; --x, ++y) {
+            if (gameState.board[x][y] === cmd.side) sum++;
+        }
+    };
+
+
 
   var handlers = {
     "CreateGame": function (cmd) {
       {
         return [{
           id: cmd.id,
+          gameId: cmd.gameId,
           event: "GameCreated",
           userName: cmd.userName,
-          timeStamp: cmd.timeStamp
+          timeStamp: cmd.timeStamp,
+          name: cmd.name
+
         }];
       }
     },
     "JoinGame": function (cmd) {
       {
-        if (gameCreatedEvent === undefined) {
+        if (gameState.gameCreatedEvent === undefined) {
           return [{
             id: cmd.id,
             event: "GameDoesNotExist",
@@ -26,16 +78,56 @@ module.exports = function tictactoeCommandHandler(events) {
           id: cmd.id,
           event: "GameJoined",
           userName: cmd.userName,
-          otherUserName: gameCreatedEvent.userName,
+          otherUserName: gameState.gameCreatedEvent.userName,
           timeStamp: cmd.timeStamp
         }];
       }
+    },
+    "MakeMove": function(cmd){
+      if(gameState.board[cmd.x][cmd.y]!==''){
+        return [{
+          id: cmd.id,
+          event: "IllegalMove",
+          userName: cmd.userName,
+          name:gameState.gameCreatedEvent.name,
+          x:cmd.x,
+          y:cmd.y,
+          side:cmd.side,
+          timeStamp: cmd.timeStamp
+        }]
+      }
+      const result = [{
+        id: cmd.id,
+        event: "MoveMade",
+        userName: cmd.userName,
+        name:gameState.gameCreatedEvent.name,
+        x:cmd.x,
+        y:cmd.y,
+        side:cmd.side,
+        timeStamp: cmd.timeStamp
+      }];
+
+      if (HasWon(cmd)) {
+          result.push({
+                    id       : cmd.id,
+                    event    : "GameWon",
+                    userName : cmd.userName,
+                    timeStamp: cmd.timeStamp
+                });
+            }
+
+
+       return result;
     }
   };
 
   return {
     executeCommand: function (cmd) {
-      return handlers[cmd.comm](cmd);
+      var handler = handlers[cmd.comm];
+      if(!handler){
+        throw new Error("No handler resolved for command " + JSON.stringify(cmd));
+      }
+      return handler(cmd);
     }
   };
 };
---
+++ server/model/tictactoe/whenMakeMove.spec.js
@@ -0,0 +1,167 @@
+var tictactoeCommandHandler = require('./tictactoeCommandHandler');
+
+describe('when make move command', function(){
+
+  var given, when, then;
+
+  beforeEach(function(){
+    given= [{
+      id:"1234",
+      event:"GameCreated",
+      name:"TheFirstGame",
+      userName: "user1",
+      timeStamp: "2015.12.02T11:29:44"
+    }, {
+      id:"12345",
+      event:"GameJoined",
+      userName: "user2",
+      otherUserName: "user1",
+      timeStamp: "2015.12.02T11:30:50"
+    }];
+  });
+
+  describe('on new game', function(){
+    it('should allow user to place anywhere on the board',function(){
+      when={
+        id:"1234",
+        comm:"MakeMove",
+        userName : "user1",
+        x:0,
+        y:1,
+        side:'X',
+        timeStamp: "2015.12.02T11:30:50"
+      };
+      then=[{
+        id:"1234",
+        event:"MoveMade",
+        userName:"user1",
+        name:"TheFirstGame",
+        x:0,
+        y:1,
+        side:'X',
+        timeStamp: "2015.12.02T11:30:50"
+      }];
+
+      var actualEvents = tictactoeCommandHandler(given).executeCommand(when);
+
+      JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
+    })
+  });
+
+  describe("on illegal duplicate move", function(){
+    it('placing move in same place should be illegal',function(){
+      given.push({
+        id:"1234",
+        event:"MoveMade",
+        userName:"user1",
+        name:"TheFirstGame",
+        x:0,
+        y:1,
+        side:'X',
+        timeStamp: "2015.12.02T11:30:50"
+      });
+
+      when={
+        id:"1234",
+        comm:"MakeMove",
+        userName : "user1",
+        x:0,
+        y:1,
+        side:'X',
+        timeStamp: "2015.12.02T11:30:50"
+      };
+
+      then=[{
+        id:"1234",
+        event:"IllegalMove",
+        userName:"user1",
+        name:"TheFirstGame",
+        x:0,
+        y:1,
+        side:'X',
+        timeStamp: "2015.12.02T11:30:50"
+      }];
+
+      var actualEvents = tictactoeCommandHandler(given).executeCommand(when);
+
+      JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
+
+    });
+  });
+
+   describe("On horizontal win", function() {
+      it("should win when three in a horizontal row", function() {
+        given.push({
+          id: "1234",
+          event: "MoveMade",
+          userName: "user1",
+          name: "TheFirstGame",
+          x: 0,
+          y: 0,
+          side: "X",
+          timeStamp: "2015.12.02T11:30:50"
+        });
+        given.push({
+          id: "12345",
+          event: "MoveMade",
+          userName: "user2",
+          name: "TheFirstGame",
+          x: 0,
+          y: 1,
+          side: "O",
+          timeStamp: "2015.12.02T11:30:50"
+        });
+        given.push({
+          id: "1234",
+          event: "MoveMade",
+          userName: "user1",
+          name: "TheFirstGame",
+          x: 1,
+          y: 0,
+          side: "X",
+          timeStamp: "2015.12.02T11:30:50"
+        });
+        given.push({
+          id: "12345",
+          event: "MoveMade",
+          userName: "user2",
+          name: "TheFirstGame",
+          x: 0,
+          y: 2,
+          side: "O",
+          timeStamp: "2015.12.02T11:30:50"
+        });
+        when = {
+          id: "1234",
+          comm: "MakeMove",
+          userName: "user1",
+          name: "TheFirstGame",
+          x: 2,
+          y: 0,
+          side: "X",
+          timeStamp: "2015.12.02T11:30:50"
+        };
+        then = [{
+        id:"1234",
+        event:"MoveMade",
+        userName: "user1",
+        name: "TheFirstGame",
+        x: 2,
+        y: 0,
+        side: "X",
+        timeStamp: "2015.12.02T11:30:50"
+      },{
+        id: "1234",
+        event: "GameWon",
+        userName: "user1",
+        timeStamp: "2015.12.02T11:30:50"
+      }];
+
+      var actualEvents = tictactoeCommandHandler(given).executeCommand(when);
+      JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
+
+      });
+   });
+
+
+});
