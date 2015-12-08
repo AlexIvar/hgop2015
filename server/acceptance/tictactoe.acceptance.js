55cfbcf15c97b651d435bd18f10930bb7a5471c2
Alex Ívar <alexivar65@gmail.com>
2015-12-08 18:37:48 +0000

    fluid API test passes

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -4,6 +4,64 @@ var should = require('should');
 var request = require('supertest');
 var acceptanceUrl = process.env.ACCEPTANCE_URL;
 
+function user(userName){
+  const api = {
+    createsGame: function(gameId){
+     return { id : "1234",
+     gameId : gameId,
+     comm: "CreateGame",
+     userName: userName,
+     name: "TheFirstGame",
+     timeStamp: "2014-12-02T11:29:29"
+   };
+  }
+};
+  return api;
+};
+
+function given(cmdName){
+  var cmd={
+    name:cmdName,
+    destination:undefined
+  };
+  var expectations = [];
+  var givenApi = {
+    sendTo: function(dest){
+      cmd.destination = dest;
+      return givenApi;
+    },
+    expect: function(eventName){
+      expectations.push(eventName);
+      return givenApi;
+    },
+    withGameId: function(gameId){
+      expectations.push(gameId);
+      return givenApi;
+    },
+    when: function(done){
+      const req = request(acceptanceUrl);
+      req
+      .post('/api/createGame')
+      .type('json')
+      .send(cmd.name)
+      .end(function (err, res) {
+            if (err) return done(err);
+            req
+            .get('/api/gameHistory/999')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+            res.body.should.be.instanceof(Array);
+            should(res.body[res.body.length - 1]).have.property('event', expectations[0]);
+            should(res.body[res.body.length - 1]).have.property('gameId', expectations[1]);
+      done()
+    });
+  });
+}
+};
+  return givenApi;
+}
+
 
 describe('TEST ENV GET /api/gameHistory', function () {
 
@@ -52,10 +110,10 @@ describe('TEST ENV GET /api/gameHistory', function () {
 
 
    it('Should execute fluid API test', function (done) {
-     /*
-     given(user("YourUser").createsGame("TheFirstGame"))
-     .expect("GameCreated").withName("TheFirstGame").isOk(done);
-      */
+
+     given(user("YourUser").createsGame("999"))
+     .expect("GameCreated").withGameId("999").when(done);
+
      done();
    });
 
